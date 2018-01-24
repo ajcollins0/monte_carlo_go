1d63fe5c0369f19abf1ca9e5c0e73b7e28747315
alex collins <alex.collins@tanium.com>
2018-01-23 23:45:24 -0500

    prep for threading

--- main.go
+++ main.go
@@ -7,66 +7,51 @@ import (
 	"time"
 )
 
-type set struct {
-	Results  float64
-	DrawDown float64
-	MaxGain  float64
-}
-
 var numTrades int = 500
-var numTests int = 1000
+var numTests int = 10
 var startingCapital int = 10000
 var percentCorrect float64 = 0.85
 var targetProfit float64 = 0.01
 var maxLoss float64 = 0.03
 
+func perform_single_test() float64 {
+	capital := float64(startingCapital)
+	for i := 0; i < numTrades; i++ {
+		randNum := rand.Int63n(100) + 1
+		if randNum <= int64(100*percentCorrect) {
+			capital = capital + (capital * targetProfit)
+		} else {
+			capital = capital - (capital * maxLoss)
+		}
+	}
+	return capital
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	var capital, drawDown, maxGain float64
-	var sets []set
-	var wins, losses int
+	var sets []float64
 	for j := 0; j < numTests; j++ {
-		wins = 0
-		losses = 0
-		capital = float64(startingCapital)
-		drawDown = capital
-		maxGain = capital
-		for i := 0; i < numTrades; i++ {
-			randNum := rand.Int63n(100) + 1
-			if randNum <= int64(100*percentCorrect) {
-				capital = capital + (capital * targetProfit)
-				if capital > maxGain {
-					maxGain = capital
-				}
-				wins++
-			} else {
-				capital = capital - (capital * maxLoss)
-				if capital < drawDown {
-					drawDown = capital
-				}
-				losses++
-			}
-		}
-		sets = append(sets, set{capital, drawDown, maxGain})
+		a := perform_single_test()
+		sets = append(sets, a)
 	}
 	printResults(sets)
 }
 
-func printResults(s []set) {
+func printResults(s []float64) {
 	var bestCase, worstCase, aveCase float64
 	var tempArray []float64
-	bestCase = s[0].Results
-	worstCase = s[0].Results
+	bestCase = s[0]
+	worstCase = s[0]
 	aveCase = 0
 	for i, _ := range s {
-		tempArray = append(tempArray, s[i].Results)
-		if s[i].Results > bestCase {
-			bestCase = s[i].Results
+		tempArray = append(tempArray, s[i])
+		if s[i] > bestCase {
+			bestCase = s[i]
 		}
-		if s[i].Results < worstCase {
-			worstCase = s[i].Results
+		if s[i] < worstCase {
+			worstCase = s[i]
 		}
-		aveCase = aveCase + s[i].Results
+		aveCase = aveCase + s[i]
 	}
 	sort.Float64s(tempArray)
 	medianCase := tempArray[(len(tempArray)/2)-1]
